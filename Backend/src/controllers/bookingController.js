7365cb518af3337936e9ce8e42ea73b2f633a626
Om Omer <omjiomer28@gmail.com>
2026-06-06 22:04:53 +0530

    updated

--- Backend/src/controllers/bookingController.js
+++ Backend/src/controllers/bookingController.js
@@ -49,4 +49,98 @@ const bookSlot = async(req,res) =>{
         })
     }
 }
-module.exports = {bookSlot}
+
+// function to get all bookings for interviewer
+const getInterviewerBookings = async(req,res) =>{
+    try{
+        // interviewer id from jwt middleware
+        const interviewerId = req.user.id
+        // fetching all the bookings for interviewer
+        const bookings = await Booking.find({
+            interviewer: interviewerId
+        })
+        .populate("slot") //optional: get slot details
+        .sort({createdAt: -1}) // latest first
+
+        return res.status(200).json({
+            message: "Interviewer bookings fetched successfully",
+            count: bookings.length,
+            bookings
+        })
+    }
+    catch(err){
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
+// function to get bookings by candidate email
+const getCandidateBookings = async (req, res) => {
+    try{
+        const {email} = req.query
+        if(!email){
+            return res.status(400).json({
+                message: "Email is required"
+            })
+        }
+        // fetching bookings for candidate
+        const bookings = await Booking.find({
+            candidateEmail: email
+        })
+        .populate("slot")
+        .sort({createdAt: -1}) //latest first
+
+        return res.status(200).json({
+            message: "Candidate bookings fetched successfully",
+            count: bookings.length,
+            bookings
+        })
+    }
+    catch(err){
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
+// function to cancel book and release slot
+const cancelBooking = async(req,res) =>{
+    try{
+        // getting booking id from req params
+        const {bookingId} = req.params
+        // finding booking
+        const booking = await Booking.findById({bookingId})
+        if(!booking){
+            return res.status(404).json({
+                message: "Booking not found"
+            })
+        }
+        //  preventing double cancellation
+        if(booking.status === "cancelled"){
+            return res.status(400).json({
+                message: "Booking already cancelled"
+            })
+        }
+        // updating booking status
+        booking.status = "cancelled"
+        await booking.save()
+
+        // releasing the slot
+        await Slot.findByIdAndUpdate(
+            booking.slot,
+            {$set: {status: "available"}}
+        )
+        // sending res
+        return res.status.json({
+            message: "Booking cancelled successfully"
+        })
+    }
+    catch(err){
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
+module.exports = {bookSlot, getInterviewerBookings, getCandidateBookings, cancelBooking}
--- Backend/src/routes/bookingRoutes.js
+++ Backend/src/routes/bookingRoutes.js
@@ -1,9 +1,19 @@
 const express = require("express")
 const router = express.Router()
 
-const {bookSlot} = require("../controllers/bookingController")
+const {bookSlot, getInterviewerBookings, getCandidateBookings, cancelBooking} = require("../controllers/bookingController")
+const authMiddleware = require("../middleware/authMiddleware")
 
 // public route for candidate booking
 router.post("/", bookSlot)
+
+// private route for getting interviewer bookings
+router.get("/interviewer", authMiddleware, getInterviewerBookings)
+
+//public route for candidate booking lookup
+router.get("/candidate",getCandidateBookings)
+
+// route to cancel booking + release slot
+router.put("/cancel/:bookingId", cancelBooking)
 
 module.exports = router
